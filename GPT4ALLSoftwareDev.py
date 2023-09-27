a06d7b3cf375c829ddd10f68b7bbef734884e554
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-09-27 10:21:48 -0400

    Update GPT4ALLSoftwareDev.py
    add docsstrings

--- GPT4ALLSoftwareDev.py
+++ GPT4ALLSoftwareDev.py
@@ -1,47 +1,65 @@
+
 from gpt4all import GPT4All, Embed4All
 from termcolor import colored
 from multiprocessing import Process, Manager, Queue
 from typing import List, Dict, Any
 
-# Initialize GPT4All and Embed4All models
-try:
-    model = GPT4All("wizardlm-13b-v1.1-superhot-8k.ggmlv3.q4_0.bin")
-    
-    embedder = Embed4All()
-except Exception as e:
-    print(f"Error initializing models: {e}")
-
-# Function to generate embeddings
-def generate_embedding(text: str) -> List[float]:
+def initialize_models():
+    """Initialize GPT4All and Embed4All models. Raise an exception if failed."""
     try:
-        if text:
-            return embedder.embed(text)
-        return []
+        model = GPT4All("wizardlm-13b-v1.1-superhot-8k.ggmlv3.q4_0.bin")
+        embedder = Embed4All()
     except Exception as e:
-        print(f"Error generating embedding: {e}")
-        return []
+        raise Exception(f"Error initializing models: {e}")
 
-# Function to safely generate text from the model
+def generate_embedding(text: str) -> List[float]:
+    """Generate embeddings for the given text using Embed4All.
+
+    Args:
+        text (str): The text for which to generate an embedding.
+
+    Returns:
+        List[float]: The generated embedding.
+
+    Raises:
+        Exception: If embedding generation fails.
+    """
+    try:
+        return embedder.embed(text) if text else []
+    except Exception as e:
+        raise Exception(f"Error generating embedding: {e}")
+
 def safe_generate(prompt: str, max_tokens: int = 50) -> str:
+    """Safely generate text using GPT4All.
+
+    Args:
+        prompt (str): The prompt to send to the model.
+        max_tokens (int, optional): The maximum number of tokens for the generated text. Defaults to 50.
+
+    Returns:
+        str: The generated text.
+
+    Raises:
+        Exception: If text generation fails.
+    """
     try:
         text = model.generate(prompt, max_tokens=max_tokens)
         return text if text else "No output from model"
     except Exception as e:
-        print(f"Error generating text: {e}")
-        return "Error generating text"
+        raise Exception(f"Error generating text: {e}")
 
-# CEO Class
 class CEO:
+    """The CEO class responsible for generating tasks for the Software Engineer."""
     def run(self, message_queue: Queue):
         try:
             ceo_request = safe_generate("Assign a task for the Software Engineer: ", 60)
             print(colored(f"=== CEO: {ceo_request} ===", 'yellow'))
             message_queue.put({"from": "CEO", "task": ceo_request, "embedding": generate_embedding(ceo_request)})
         except Exception as e:
-            print(f"Error in CEO run: {e}")
+            raise Exception(f"Error in CEO run: {e}")
 
-# Software Engineer Class
 class SoftwareEngineer:
+    """The Software Engineer class responsible for breaking down tasks into sub-tasks."""
     def run(self, message_queue: Queue):
         try:
             ceo_message = message_queue.get()
@@ -50,10 +68,10 @@ class SoftwareEngineer:
             print(colored(f"=== Software Engineer: {sub_tasks} ===", 'green'))
             message_queue.put({"from": "Software Engineer", "sub_tasks": sub_tasks, "embedding": generate_embedding(sub_tasks)})
         except Exception as e:
-            print(f"Error in Software Engineer run: {e}")
+            raise Exception(f"Error in Software Engineer run: {e}")
 
-# Code Generator Class
 class CodeGenerator:
+    """The Code Generator class responsible for generating code snippets for sub-tasks."""
     def run(self, message_queue: Queue):
         try:
             software_engineer_message = message_queue.get()
@@ -62,42 +80,40 @@ class CodeGenerator:
             print(colored(f"=== Code Generator: {code_snippets} ===", 'blue'))
             message_queue.put({"from": "Code Generator", "code_snippets": code_snippets, "embedding": generate_embedding(code_snippets)})
         except Exception as e:
-            print(f"Error in Code Generator run: {e}")
+            raise Exception(f"Error in Code Generator run: {e}")
 
-# Data Analyst Class
 class DataAnalyst:
+    """The Data Analyst class responsible for analyzing data and providing insights."""
     def run(self, message_queue: Queue):
         try:
             insights = safe_generate("Analyze the data and provide insights: ", 120)
             print(colored(f"=== Data Analyst: {insights} ===", 'yellow'))
             message_queue.put({"from": "Data Analyst", "insights": insights, "embedding": generate_embedding(insights)})
         except Exception as e:
-            print(f"Error in Data Analyst run: {e}")
+            raise Exception(f"Error in Data Analyst run: {e}")
 
-# QA Engineer Class
 class QAEngineer:
+    """The QA Engineer class responsible for performing tests and providing results."""
     def run(self, message_queue: Queue):
         try:
             test_results = safe_generate("Perform tests and provide results: ", 120)
             print(colored(f"=== QA Engineer: {test_results} ===", 'red'))
             message_queue.put({"from": "QA Engineer", "test_results": test_results, "embedding": generate_embedding(test_results)})
         except Exception as e:
-            print(f"Error in QA Engineer run: {e}")
+            raise Exception(f"Error in QA Engineer run: {e}")
 
-# Main Execution
 if __name__ == '__main__':
     try:
+        initialize_models()
         with Manager() as manager:
             message_queue = manager.Queue()
             roles = [CEO(), SoftwareEngineer(), CodeGenerator(), DataAnalyst(), QAEngineer()]
-
             for role in roles:
                 role_thread = Process(target=role.run, args=(message_queue,))
                 role_thread.start()
                 role_thread.join()
-
             code_generator_message = message_queue.get()
             code_snippets = code_generator_message["code_snippets"]
             print(colored(f"=== Generated Python Program: {code_snippets} ===", 'magenta'))
     except Exception as e:
-        print(f"Error in main execution: {e}")
+        raise Exception(f"Error in main execution: {e}")
